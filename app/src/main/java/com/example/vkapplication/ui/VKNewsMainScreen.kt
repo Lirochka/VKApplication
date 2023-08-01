1bd514662dd4101017a4282a19df54f9321b510d
Lira <lir0na@mail.ru>
2023-08-01 20:41:54 +0300

    Created UI screen

---
+++ app/src/main/java/com/example/vkapplication/ui/VKNewsMainScreen.kt
@@ -0,0 +1,53 @@
+package com.example.vkapplication.ui
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun MainScreen() {
+
+    Scaffold(
+        bottomBar = {
+            BottomNavigation() {
+                val selectedItemPosition = remember {
+                    mutableStateOf(0)
+                }
+
+                val items = listOf(
+                    NavigationItem.Home,
+                    NavigationItem.Favourite,
+                    NavigationItem.Profile
+                )
+                items.forEachIndexed { index, item ->
+                    BottomNavigationItem(
+                        selected = selectedItemPosition.value == index,
+                        onClick = { selectedItemPosition.value = index },
+                        icon = { Icon(item.icon, contentDescription = null) },
+                        label = {
+                            Text(text = stringResource(id = item.titleResId))
+                        },
+                        selectedContentColor = MaterialTheme.colors.onPrimary,
+                        unselectedContentColor = MaterialTheme.colors.onSecondary
+                    )
+                }
+            }
+        }
+    ) { PostCard(
+        modifier = Modifier.padding(8.dp)) }
+}
+
+
